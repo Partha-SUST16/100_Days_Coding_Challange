6af28a0a79510309aa43b6ecf92bd0be08c3ac6b
Partha-SUST16 <partha05@student.sust.edu>
2020-01-26 22:12:35 +0600

    String to number

--- LeetCode/String to Number.cpp
+++ LeetCode/String to Number.cpp
@@ -6,25 +6,73 @@ class Solution
 public:
     int myAtoi(string str)
     {
-        int i = 0;
-        while (str[i] == ' ')
-            i++;
-        int sign = 1, result = 0;
-        if (str[i] == '-' || str[i] == '+')
-            sign = (str[i++] == '+') ? 1 : -1;
+        //         int i = 0;
+        //     while(str[i]==' ')
+        //         i++;
+        //     int sign =1,result = 0;
+        //     if(str[i]=='-' || str[i]=='+')
+        //         sign = (str[i++]=='+')?1:-1;
 
-        while (str[i] >= '0' and str[i] <= '9')
+        //     while (str[i]>='0' and str[i]<='9')
+        //     {
+        //         if(result>INT_MAX/10 or (result==INT_MAX/10 and str[i]-'0'>7))
+        //         {
+        //             if(sign==1)
+        //                 return INT_MAX;
+        //             else
+        //                 return INT_MIN;
+        //         }
+        //         result = result*10+(str[i++]-'0');
+        //     }
+        //     return result*sign;
+        //stringstream s(str);
+        // int a =0;
+        // s >> a;
+        // return a;
+        if (str.empty())
+            return 0;
+
+        // remove whitespaces
+        int i = 0;
+        while (i < str.size() && str[i] == ' ')
+            ++i;
+
+        // after whitespace removal if the first character is anything
+        // other than +,-,[0-9]
+        char first_char = str[i++];
+        bool negative = false;
+        int result = 0;
+
+        if (first_char != '-' && first_char != '+' &&
+            ((first_char - '0') < 0 || (first_char - '0') > 9))
+            return 0;
+
+        // set sign
+        negative = first_char == '-' ? true : false;
+        // if first char is a digit instead of sign
+        result = ((first_char - '0') >= 0 && (first_char - '0') <= 9) ? (first_char - '0') : 0;
+
+        // construct the remaining number using chars
+        // if the final number is negative then each time we add negative digits
+        // this makes it possible to save it in 32-bit int otherwise overflow occurs
+        while (i < str.size() && isdigit(str[i]))
         {
-            if (result > INT_MAX / 10 or (result == INT_MAX / 10 and str[i] - '0' > 7))
-            {
-                if (sign == 1)
-                    return INT_MAX;
-                else
-                    return INT_MIN;
-            }
-            result = result * 10 + (str[i++] - '0');
+            int curr_digit = str[i] - '0';
+
+            // check if the number goees beyond negative LIMIT
+            if (negative && (result < (INT_MIN + curr_digit) / 10))
+                return INT_MIN;
+            // check if the number goees beyond positive LIMIT
+            else if (!negative && (result > (INT_MAX - curr_digit) / 10))
+                return INT_MAX;
+
+            // make the current digit negative in case of negative number is final result
+            if (negative)
+                curr_digit *= -1;
+            result = result * 10 + curr_digit;
+            ++i;
         }
-        return result * sign;
+        return result;
     }
 };
 
