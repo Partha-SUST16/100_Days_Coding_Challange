ff1ad9e9d52a6be242cb973a9f2e8b4a3f2c2556
Partha Protim Paul <41873352+Partha-SUST16@users.noreply.github.com>
2020-05-17 00:27:55 +0600

    Div2- 643 (a,b,c,d)

---
+++ CodeForces/From Begining/Div2-16-05-20/Count Trianles.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <queue>
+#include <map>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <limits.h>
+#include <bits/stdc++.h>
+#define fw(x) freopen("x.txt", "w", stdout)
+#define For(i, a, b, c) for (int i = a; i < b; i += c)
+#define REP(i, n) for (int i = 0; i < n; ++i)
+#define REP1(i, n) for (int i = 1; i <= n; i++)
+#define mem(a, s) memset(a, s, sizeof a)
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(v) v.begin(), v.end()
+#define vii vector<int>
+#define vll vector<ll>
+#define vss vector<string>
+#define debug(x) cout << #x "=" << (x) << endl
+#define pb push_back
+
+using namespace std;
+
+// moves
+
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*knight move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*knight move*/
+
+//big_mod
+
+//ll bigmod(ll a,ll b,ll m)
+//{if(b == 0) return 1%m;ll x = bigmod(a,b/2,m);x = (x * x) % m;if(b % 2 == 1) x = (x * a)% m;return x;}
+//ll BigMod(ll B,ll P,ll M){ ll R=1%M; while(P>0) {if(P%2==1){R=(R*B)%M;}P/=2;B=(B*B)%M;} return R;} /// (B^P)%M
+
+//ll getBit(ll num, int idx) {return ((num >> idx) & 1ll) == 1ll;}
+//ll setBit1(ll num, int idx) {return num or (1ll<<idx);}
+//ll setBit0(ll num, int idx) {return num & ~(1ll<<idx);}
+//ll flipBit(ll num, int idx) {return num ^ (1ll<<idx);}
+
+const int N = (int)1500002;
+const int mod = 1000000007;
+//ll qpow(ll n,ll k) {ll ans=1;assert(k>=0);n%=mod;while(k>0){if(k&1) ans=(ans*n)%mod;n=(n*n)%mod;k>>=1;}return ans%mod;}
+//used tutorial
+ll dp[4][N],ps[4][N];
+int main()
+{
+    ll a,b,c,d;
+    cin >> a >> b >> c >> d;
+    for(ll i = a; i<=b; i++)
+        dp[1][i] = 1;
+    for(ll i = 1; i<=N-2; i++)
+        ps[1][i] = dp[1][i]+ps[1][i-1];
+    for(ll i = 1; i<=N-2; i++)
+    {
+        ll valX = 0;
+        if(i>=b)
+            valX = ps[1][i-b];
+        if(i>c)
+            valX -= ps[1][i-c-1];
+        dp[2][i] =valX;
+        ps[2][i] = ps[2][i-1] + dp[2][i];
+
+    }
+    ll ans = 0;
+    for(ll i=1; i<=N-2; i++)
+    {
+        if(dp[2][i]!=0)
+        {
+            int min_ = c;
+            int max_ = min(d,i-1);
+            ans += 1LL*dp[2][i]*max(0,max_-min_+1);
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
