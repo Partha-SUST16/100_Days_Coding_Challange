edf3bfe1280e951daa71acbf293504c27caf57ed
Partha Protim Paul <41873352+Partha-SUST16@users.noreply.github.com>
2020-05-20 01:23:44 +0600

    Begin from where i left

---
+++ CodeForces/From Begining/Div3 - 515/Binary Number and Sum.cpp
@@ -0,0 +1,103 @@
+#include <cstdlib>
+#include <cctype>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <fstream>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <list>
+#include <stdexcept>
+#include <functional>
+#include <utility>
+#include <ctime>
+#include <random>
+#include <cassert>
+#include <bits/stdc++.h>
+#define fw(x) freopen("x.txt", "w", stdout)
+#define For(i, a, b, c) for (int i = a; i < b; i += c)
+#define REP(i, n) for (int i = 0; i < n; ++i)
+#define REP1(i, n) for (int i = 1; i <= n; i++)
+#define mem(a, s) memset(a, s, sizeof a)
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(v) v.begin(), v.end()
+#define vii vector<int>
+#define vll vector<ll>
+#define vss vector<string>
+#define debug(x) cout << #x "=" << (x) << endl
+#define pb push_back
+
+using namespace std;
+
+// moves
+
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*knight move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*knight move*/
+
+//big_mod
+
+//ll bigmod(ll a,ll b,ll m)
+//{if(b == 0) return 1%m;ll x = bigmod(a,b/2,m);x = (x * x) % m;if(b % 2 == 1) x = (x * a)% m;return x;}
+//ll BigMod(ll B,ll P,ll M){ ll R=1%M; while(P>0) {if(P%2==1){R=(R*B)%M;}P/=2;B=(B*B)%M;} return R;} /// (B^P)%M
+
+//ll getBit(ll num, int idx) {return ((num >> idx) & 1ll) == 1ll;}
+//ll setBit1(ll num, int idx) {return num or (1ll<<idx);}
+//ll setBit0(ll num, int idx) {return num & ~(1ll<<idx);}
+//ll flipBit(ll num, int idx) {return num ^ (1ll<<idx);}
+
+const int N = (int)2e5 + 5;
+const int mod = 998244353;
+//ll qpow(ll n,ll k) {ll ans=1;assert(k>=0);n%=mod;while(k>0){if(k&1) ans=(ans*n)%mod;n=(n*n)%mod;k>>=1;}return ans%mod;}
+ll p2[N];
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    string a,b;
+    cin >> a >> b;
+    int maxLen = max(a.length(),b.length());
+    reverse(all(a));
+    reverse(all(b));
+    a+=string(maxLen-a.length(),'0');
+    b+=string(maxLen-b.length(),'0');
+    int countOne = count(all(b),'1');
+
+    p2[0] = 1;
+    for(int i=1; i<N; i++)
+    {
+        p2[i] =p2[i-1]*2;
+        if(p2[i]>=mod)p2[i]-=mod;
+    }
+    ll ans = 0;
+    for(int i=0; i<maxLen; i++)
+    {
+        if(a[i]=='1')
+        {
+            ans += 1LL * p2[i]*countOne %mod;
+        }
+        countOne -= (b[i]-'0');
+    }
+    cout<<ans%mod<<endl;
+    return 0;
+}
