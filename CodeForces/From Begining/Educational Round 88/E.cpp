b27d3ed48002fd568fe94de78118cc58f4efdf7a
Partha Protim Paul <41873352+Partha-SUST16@users.noreply.github.com>
2020-05-30 21:46:30 +0600

    e

---
+++ CodeForces/From Begining/Educational Round 88/E.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <queue>
+#include <map>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <limits.h>
+#include <bits/stdc++.h>
+#define fw(x) freopen("x.txt", "w", stdout)
+#define For(i, a, b, c) for (int i = a; i < b; i += c)
+#define REP(i, n) for (int i = 0; i < n; ++i)
+#define REP1(i, n) for (int i = 1; i <= n; i++)
+#define mem(a, s) memset(a, s, sizeof a)
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(v) v.begin(), v.end()
+#define vii vector<int>
+#define vll vector<ll>
+#define vss vector<string>
+#define debug(x) cout << #x "=" << (x) << endl
+#define pb push_back
+
+using namespace std;
+
+// moves
+
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*knight move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*knight move*/
+
+//big_mod
+
+//ll bigmod(ll a,ll b,ll m)
+//{if(b == 0) return 1%m;ll x = bigmod(a,b/2,m);x = (x * x) % m;if(b % 2 == 1) x = (x * a)% m;return x;}
+//ll BigMod(ll B,ll P,ll M){ ll R=1%M; while(P>0) {if(P%2==1){R=(R*B)%M;}P/=2;B=(B*B)%M;} return R;} /// (B^P)%M
+
+//ll getBit(ll num, int idx) {return ((num >> idx) & 1ll) == 1ll;}
+//ll setBit1(ll num, int idx) {return num or (1ll<<idx);}
+//ll setBit0(ll num, int idx) {return num & ~(1ll<<idx);}
+//ll flipBit(ll num, int idx) {return num ^ (1ll<<idx);}
+template <class T> inline T bigmod(T p,T e,T M){
+    ll ret = 1;
+    for(; e > 0; e >>= 1){
+        if(e & 1) ret = (ret * p) % M;
+        p = (p * p) % M;
+    }
+    return (T)ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+template <class T> inline T modinverse(T a,T M){return bigmod(a,M-2,M);}
+const ll mod = 998244353ll;
+
+ll fact[555555], inv[555555];
+ll ncr(int n, int r){
+  return (((fact[n] * inv[r]) % mod) * inv[n-r]) % mod;
+}
+
+
+int main(){
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  fact[0] = 1;
+  REP1(i, 555554) fact[i] = (fact[i-1] * (ll)i) % mod;
+  REP(i, 555555) inv[i] = modinverse(fact[i], mod);
+  int n, k; cin >> n >> k;
+  ll res = 0;
+  for(int i = 1; i <= n; i++){
+    int v = (n / i) - 1;
+    if(v >= k-1) res = (res + ncr(v, k-1)) % mod;
+  }
+  cout << res << endl;
+}
